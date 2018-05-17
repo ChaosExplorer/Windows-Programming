7dea7c07e791eb64fabae27fd5af3d4b49a928b2
Chao Song <songcom@live.com>
2018-05-17 23:14:04 +0800

    Update Filter.cpp

--- samhandler/core/Filter.cpp
+++ samhandler/core/Filter.cpp
@@ -2,16 +2,28 @@
 // Created by chaos on 5/16/18.
 //
 
+#include <cstdio>
 #include "Filter.h"
+#include "Repository.h"
 
 void Filter::goForAll(char *buffer, sortIndecVec &indecVec, StasticsData &stat) {
     off64_t header_end_pos = 0;
     off64_t body_start_pos = 0;
-    off64_t record_head;
-    off64_t record_tail;
+    off64_t record_head = 0;
+    off64_t record_tail = 0;
 
-    while (getline(buffer, )) {
+    off_t find_start_pos = 0;
 
+    char* tag;
+    while ((tag = strchr(&buffer[find_start_pos]),'\n') != NULL) {
+        find_start_pos = tag - buffer + 1;
+        if (buffer[find_start_pos] != '@')
+            break;
     }
-
+    header_end_pos = find_start_pos - 1;
+    
+    while ((tag = strchr(&buffer[find_start_pos]),'\n') != NULL) {
+        find_start_pos = tag - buffer + 1;
+        
+    }
 }
