9d200056a7e61dc938708c6cea960a8fb6ba8071
chaos <songchao1@genomics.cn>
2018-06-27 20:13:44 +0800

    add SOAP

---
+++ SOAP/CMakeFiles/SOAPnuke.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/Common.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/Common.cpp.o"
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/DGECommon.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/DGECommon.cpp.o"
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/DGEProcessor.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/DGEProcessor.cpp.o"
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/FilterProcessor.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/FilterProcessor.cpp.o"
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/FqBuffer.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/FqBuffer.cpp.o"
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/FqFile.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/FqFile.cpp.o"
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/Logger.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/Logger.cpp.o"
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/Main.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/Main.cpp.o"
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/MetaCommon.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/MetaCommon.cpp.o"
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/MetaProcessor.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/MetaProcessor.cpp.o"
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/PeBuffer.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/PeBuffer.cpp.o"
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/SRNACleanFA.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/SRNACleanFA.cpp.o"
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/SRNACleanFQ.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/SRNACleanFQ.cpp.o"
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/SRNACommon.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/SRNACommon.cpp.o"
+  "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/SRNAProcessor.cpp" "/home/chaos/projects/bgi_practice/SOAPnuke-bak/bgi_SOAPnuke/src/CMakeFiles/SOAPnuke.dir/SRNAProcessor.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
